63bd5f24573c67c71dc1f414891413be55d89089
gomgomgit <thechocohead@gmail.com>
2020-07-26 16:33:59 +0700

    make category controller

--- foodcourt/app/Http/Controllers/CategoryController.php
+++ foodcourt/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Category
 
 class CategoryController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->model = new Category();
+        $this->redirect = '/categories';
+        $this->view = 'category';
+    }
+
+    public function index()
+    {
+        $datas = $this->model->all();
+
+        return view($this->view . '.index', compact('datas'));
+    }
+    public function create()
+    {
+        return view($this->view . '.create');
+    }
+    public function store(Request $request)
+    {
+        $this->model->create($request->all());
+        return redirect($this->redirect);
+    }
+    public function edit($id)
+    {
+        $data = $this->model->find($id);
+        return view($this->view . '.edit', compact('data'));
+    }
+    public function update(Request $request, $id)
+    {
+        $this->model->find($id)->update($request->all());
+        return redirect($this->redirect);
+    }
+    public function delete($id)
+    {
+        $this->model->find($id)->delete();
+        return redirect($this->redirect);
+    }
 }
